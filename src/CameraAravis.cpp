aaf6e2d8164f48c2e7460051d12f2026d77e3b9f
Leif Christensen <leif.christensen@dfki.de>
2016-01-14 18:27:01 +0100

    CameraAravis changed double comparison from throwing run time error, when the camera sets the desired fps to the nearest possible value

--- src/CameraAravis.cpp
+++ src/CameraAravis.cpp
@@ -318,7 +318,7 @@ namespace camera
 		            arv_camera_stop_acquisition(camera); 
                             arv_camera_set_frame_rate(camera, value);
                             double fps = arv_camera_get_frame_rate(camera);
-			    if (fps != value) throw runtime_error("The attribute 'FrameRate' could not be set");
+			    if (fabs(fps - value) > 0.01) throw runtime_error("The attribute 'FrameRate' could not be set");
 			    arv_camera_start_acquisition(camera);
                             return true;
 		    }
